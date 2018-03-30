1376bb00ba9b86c9e8a1716372d7f8ee47890363
FearlessTobi <thm.frey@gmail.com>
2018-03-31 00:39:30 +0200

    Show game compatibility within Citra

--- src/citra_qt/main.cpp
+++ src/citra_qt/main.cpp
@@ -128,6 +128,7 @@ GMainWindow::GMainWindow() : config(new Config()), emu_thread(nullptr) {
 
     show();
 
+    game_list->LoadCompatibilityList();
     game_list->PopulateAsync(UISettings::values.gamedir, UISettings::values.gamedir_deepscan);
 
     default_theme_paths = QIcon::themeSearchPaths();
